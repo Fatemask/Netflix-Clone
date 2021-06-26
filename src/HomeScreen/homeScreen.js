252e63a0211facbfc54f511fe6cffb353588c3ac
Fatemask <fatemask29@gmail.com>
2021-06-26 11:35:30 +0530

    res

--- src/HomeScreen/homeScreen.js
+++ src/HomeScreen/homeScreen.js
@@ -13,12 +13,12 @@ function HomeScreen (){
          <Banner />
          <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true} />
          <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow={true} />
-         <Row title="Top Rated" fetchUrl={requests.fetchTopRated} isLargeRow={true} />
-         <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} isLargeRow={true} />
-         <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} isLargeRow={true} />
-         <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} isLargeRow={true} />
-         <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} isLargeRow={true} />
-         <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} isLargeRow={true} />
+         <Row title="Top Rated" fetchUrl={requests.fetchTopRated} isLargeRow={false} />
+         <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} isLargeRow={false} />
+         <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} isLargeRow={false} />
+         <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} isLargeRow={false} />
+         <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} isLargeRow={false} />
+         <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} isLargeRow={false} />
          <Footer/>
      </div>
  )
